6d0ae9fe7a67d65cded7f110417ed69764edd080
daniel <daniel@freshmilk.tv>
2012-08-03 09:51:36 +0200

    Add slgugify to pages form field slug

--- web/app/views/pages/forms.py
+++ web/app/views/pages/forms.py
@@ -3,6 +3,7 @@
 import datetime
 from flask.ext.wtf import Form, Required, Optional, BooleanField, \
     IntegerField, TextField, TextAreaField
+from app.views.backpack.utils import slugify
 
 class BaseForm(Form):
     author = TextField(validators=[Required()])
@@ -15,9 +16,15 @@ class BaseForm(Form):
 
 class PageForm(BaseForm):
     title = TextField(validators=[Required()])
-    slug = TextField(validators=[Required()])
+    slug = TextField()
     teaser = TextAreaField()
     text = TextAreaField(validators=[Required()])
 
     exclude = ['csrf_token', 'created', 'author']
     editor = ['teaser', 'text']
+
+    def validate_slug(form, field):
+        if field.data:
+            field.data = slugify(field.data)
+        else:
+            field.data = slugify(form.data['title'])
