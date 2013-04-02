bd27b33c00b72f423941e73573eec66ff62a7ffd
haengebruegge <me@haengebruegge.de>
2013-04-02 21:27:18 +0200

    Small rewrite

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, g, send_file, render_template
+from flask.ext.assets import Environment
+from blueprints import add_blueprints
 
 
 def create_app(settings):
     app = Flask(__name__)
     app.config.from_pyfile(settings)
+
+    assets = Environment()
+    assets.init_app(app)
 
     if not app.debug:
         import logging
@@ -17,6 +22,8 @@ def create_app(settings):
         )
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+    add_blueprints(app)
 
     @app.errorhandler(404)
     def page_not_found(e):
--- app/blueprints/__init__.py
+++ app/blueprints/__init__.py
@@ -3,11 +3,11 @@ from werkzeug.utils import import_string, ImportStringError
 
 def add_blueprints(app):
     for module in app.config['BLUEPRINTS']:
-        bp = import_string('app.views.' + module + '.bp')
+        bp = import_string('app.blueprints.' + module + '.bp')
         app.register_blueprint(bp)
 
         try:
-            admin = import_string('app.views.' + module + '.admin')
+            admin = import_string('app.blueprints.' + module + '.admin')
             app.register_blueprint(admin)
         except ImportStringError:
             print module + ' has no admin blueprint.'
