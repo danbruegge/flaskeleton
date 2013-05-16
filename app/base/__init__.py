a895440b937537d3f8373b4b27f9bab35fea3a6b
haengebruegge <me@haengebruegge.de>
2013-05-16 19:56:08 +0200

    Fixes.

--- app/base/__init__.py
+++ app/base/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template
+from flask import Blueprint
 
 bp = Blueprint(
     'base',
@@ -7,11 +7,3 @@ bp = Blueprint(
     template_folder='templates',
     static_folder='static'
 )
-
-@bp.route('/base')
-def base():
-    print dir(bp)
-    print bp.static_folder
-    print bp.static_url_path
-
-    return 'hello world'
