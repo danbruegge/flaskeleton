ed5a9cc9f7c4db41a8486fa18c64420db5baea45
haengebruegge <me@haengebruegge.de>
2013-07-25 20:07:35 +0200

    Update

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,10 @@
 from fabric.api import *
 
 
+def pip_install():
+    local('pip install -U -r requirements.txt')
+
+
 def pip_update():
     local('pip freeze --local | grep -v "^\-e" | cut -d = -f 1  | xargs pip install -U')
 
