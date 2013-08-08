100e5ca433fed69c87ee8044572dec14a7869ffd
haengebruegge <me@haengebruegge.de>
2013-08-08 20:25:12 +0200

    Get dropbox file download to work

--- app/api/views.py
+++ app/api/views.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, redirect, url_for, jsonify
+from flask import Blueprint, redirect, url_for, request, jsonify
 from flask import current_app as app
 from app import dropbox
 from app.base.utils import ensure_dir
@@ -8,10 +8,14 @@ from app.base.utils import ensure_dir
 bp = Blueprint('api', __name__, url_prefix='/api')
 
 
-@bp.route('/')
+@bp.route('/', methods=['GET'])
 def auth():
-    return u'Click <a href="%s">here</a> to login with Dropbox.'\
-           % dropbox.login_url
+    if not dropbox.is_authenticated:
+        return u'Click <a href="%s">here</a> to login with Dropbox.'\
+               % dropbox.login_url
+
+    return 'Logged in! View json: <a href="%s">here</a>!' % url_for('.db')
+
 
 
 @bp.route('/db/')
@@ -19,35 +23,31 @@ def db():
     if not dropbox.is_authenticated:
         return redirect(url_for('.auth'))
 
-    items = dropbox.client.metadata('/marc-images/Portfolio 1/')
+    items = dropbox.client.metadata('/marc-images')
 
     get_dropbox_files(items)
 
     return jsonify(items=items)
 
 
-def create_dropbox_file(path):
+def create_dropbox_file(local_path, path):
     """Create a file from dropbox
     """
-    print path
-    out = open(path, 'w')
-    f = dropbox.client.get_file(path)
-    out.write(f.read())
+    with open(local_path, 'wb') as f:
+        data = dropbox.client.get_file(path)
+        f.write(data.read())
 
 
 def get_dropbox_files(items):
     for item in items['contents']:
         # backslash is needed to create the folders
-        path = '%s%s' % (app.config['MEDIA_ROOT'], item['path'])
+        local_path = '%s%s' % (app.config['MEDIA_ROOT'], item['path'])
 
-        ensure_dir(path)
+        ensure_dir(local_path)
 
         # if current item is a folder
         if item['is_dir']:
-            path += '/'
-
-            subs = dropbox.client.metadata(path)
-            print subs
+            subs = dropbox.client.metadata('%s/' % item['path'])
             get_dropbox_files(subs)
         else:
-            create_dropbox_file(path)
+            create_dropbox_file(local_path, item['path'])
