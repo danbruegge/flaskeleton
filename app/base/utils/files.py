499be1ed05f214d79bd9cadb25c4afe9701adc23
haengebruegge <me@haengebruegge.de>
2013-11-10 20:10:05 +0100

    Clean up and Updates

--- app/base/utils/files.py
+++ app/base/utils/files.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
+import os
 
 
 def ensure_dir(path):
-    """Check if a dir exists and if not, create it.
-    """
+    """Check if a dir exists and if not, create it."""
     import os
 
     d = os.path.dirname(path)
     if not os.path.exists(d):
         os.makedirs(d)
+
+
+def extend_filename(filename, extend):
+    """Extend a filename at the end but before the extension."""
+    # split image path in path and image name
+    path, name = os.path.split(filename)
+
+    # add name addition
+    name = name.replace('.', '%s.' % extend)
+
+    # put all together and return
+    return os.path.join(path, name)
