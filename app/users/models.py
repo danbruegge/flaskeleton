fc5765509bbc7cf8c0961c181cdf3c16fbf79e26
haengebruegge <me@haengebruegge.de>
2014-05-21 21:32:14 +0200

    Fix forgotten parameter in check_password_hash

--- app/users/models.py
+++ app/users/models.py
@@ -19,7 +19,7 @@ class User(UserMixin):
         self.password = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(password)
+        return check_password_hash(self.password, password)
 
     def is_active(self):
         return self.active
