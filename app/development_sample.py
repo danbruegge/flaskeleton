3f344d0ee96a004f90e6f3b781145bf308a3bc9f
haengebruegge <me@haengebruegge.de>
2014-05-21 19:13:48 +0200

    Remove options from development sample

--- app/development_sample.py
+++ app/development_sample.py
@@ -8,16 +8,3 @@ ASSETS_DEBUG = DEBUG
 
 DEBUG_TB_PROFILER_ENABLED = DEBUG
 DEBUG_TB_INTERCEPT_REDIRECTS = False
-
-# Users app
-# TODO: redundant if users app uses a database
-if 'users' in BLUEPRINTS:
-    from app.users.models import User
-
-    USERS = {
-        'admin': User(
-            'admin',
-            'password please secure me',
-            1
-        )
-    }
